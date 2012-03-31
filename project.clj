47f7a3ecb0a16ae45a48942f969638817c453ec4
Denis Labaye <denis.labaye@gmail.com>
2012-03-31 15:14:09 +0200

    deps: added apache camel deps

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject apache-camel-clojure "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.apache.camel/camel-core "2.9.0"]]
+  :dev-dependencies [[midje "1.3.1"]
+                     [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]])
