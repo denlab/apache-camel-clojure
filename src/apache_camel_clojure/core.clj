de8f33249b250993ff7f4c034d29e7b6c1983556
Denis Labaye <denis.labaye@gmail.com>
2012-03-31 14:35:39 +0200

    initialized clj project

---
+++ src/apache_camel_clojure/core.clj
@@ -0,0 +1,1 @@
+(ns apache-camel-clojure.core)
---
+++ test/apache_camel_clojure/test/core.clj
@@ -0,0 +1,6 @@
+(ns apache-camel-clojure.test.core
+  (:use [apache-camel-clojure.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
