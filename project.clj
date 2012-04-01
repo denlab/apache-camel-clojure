f3f137f25ea5011c43817ecefa5eb5698593117e
Denis Labaye <denis.labaye@gmail.com>
2012-04-01 14:16:42 +0200

    deps: added jms and active mq deps

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject apache-camel-clojure "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.camel/camel-core "2.9.0"]]
+                 [org.apache.camel/camel-core       "2.9.0"]
+                 [org.apache.camel/camel-jms        "2.9.0"]
+                 [org.apache.activemq/activemq-core "5.5.1"]]
   :dev-dependencies [[midje "1.3.1"]
                      [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]])
